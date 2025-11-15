2ac2c096f43f7894feb3586e40f640f74f367fb5
Rallabandi Varshith <136003240+varshithanonymous@users.noreply.github.com>
2025-11-15 13:07:27 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import Header from '@/components/Header'
-import Hero from '@/components/Hero'
-import About from '@/components/About'
-import Projects from '@/components/Projects'
-import Contact from '@/components/Contact'
+import Header from '/components/Header'
+import Hero from '/components/Hero'
+import About from '/components/About'
+import Projects from '/components/Projects'
+import Contact from '/components/Contact'
 
 export default function Home() {
   return (
